19abb987a718e04865b9557c7eb042c75b1d89d0
sujon157 <riponahamed1579@gmail.com>
2024-03-20 06:00:08 +0600

    my 2nd commit

--- src/components/Phones/Phones.jsx
+++ src/components/Phones/Phones.jsx
@@ -1,11 +1,12 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
-
+import { BarChart, Bar, XAxis,YAxis ,Tooltip} from 'recharts';
+import { Audio } from 'react-loader-spinner';
+import { Grid } from "react-loader-spinner";
 const Phones = () => {
 
     const [phones, setPhones] = useState([]);
-
+    const [loading, setLoading] = useState([true]);
     useEffect(() => {
 
         // fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
@@ -24,13 +25,38 @@ const Phones = () => {
                     return obj;
                 })
                 console.log(phoneWithFakeData);
-                setPhones(phoneWithFakeData)
+                setPhones(phoneWithFakeData);
+                setLoading(false);
             });
 
 
     }, [])
     return (
         <div>
+            {loading && <div>
+
+                <Audio
+                    height="80"
+                    width="80"
+                    radius="9"
+                    color="green"
+                    ariaLabel="loading"
+                    wrapperStyle
+                    wrapperClass
+                />
+
+                <Grid
+                    visible={true}
+                    height="80"
+                    width="80"
+                    color="#4fa94d"
+                    ariaLabel="grid-loading"
+                    radius="12.5"
+                    wrapperStyle={{}}
+                    wrapperClass="grid-wrapper"
+                />
+            </div>
+            }
             <h2 className="text-5xl">Phones:{phones.length} </h2>
 
             <BarChart width={1200} height={600} data={phones}>
